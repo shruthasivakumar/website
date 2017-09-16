99017f0755c56eddd52849af1c115e6449902da6
Devvart Poddar <devvart123@gmail.com>
2017-09-17 00:21:33 +0200

    travis login

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,10 +2,10 @@
 set -x # Show the output of the following commands (useful for debugging)
 
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_04c508659a90_key -iv $encrypted_04c508659a90_iv -in travisci-key.enc -out travisci-key -d
-rm travisci-key.enc # Don't need it anymore
-chmod 600 travisci-key
-mv travisci-key ~/.ssh/id_rsa
+openssl aes-256-cbc -K $encrypted_cdc79972ae8a_key -iv $encrypted_cdc79972ae8a_iv -in shrutha-key.enc -out shrutha-key -d
+rm shrutha-key.enc # Don't need it anymore
+chmod 600 shrutha-key
+mv shrutha-key ~/.ssh/id_rsa
 
 # Install zopfli
 git clone https://github.com/google/zopfli.git
